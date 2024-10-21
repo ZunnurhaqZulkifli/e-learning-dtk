7fcee4ab57fbc18217c7e5543950afa762d54dc3
ZunnurhaqZulkifli <zunnurclash24@gmail.com>
2024-10-22 01:16:20 +0800

    updated image carousell

--- resources/js/Pages/Components/CarouselImage.jsx
+++ resources/js/Pages/Components/CarouselImage.jsx
@@ -2,7 +2,7 @@ export function CarouselImage({ text, image }) {
   return (
     <img
       className="d-block w-100"
-      src={`http://e-learning.test/`+ image}
+      src={`http://e-learning-dtk.test/`+ image}
       // alt={image}
     />
   );
